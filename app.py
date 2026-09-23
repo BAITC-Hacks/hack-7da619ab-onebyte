fe0e66d2e855e461988916dca99ba9a2e75353c8
Arai Otegali <arai.otegali@gmail.com>
2026-09-23 13:53:53 +0500

    Add FastAPI backend

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Meeting AI started!")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {
+        "name": "Meeting AI",
+        "status": "working",
+        "message": "Система автопротоколирования совещаний"
+    }
